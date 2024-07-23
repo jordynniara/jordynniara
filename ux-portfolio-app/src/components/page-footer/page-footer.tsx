7ac6b72d2a8116d56ada974e0c244bcac3f8325f
jordynniara <jnsmith96@yahoo.com>
2024-07-22 22:49:02 -0400

    created footer

--- ux-portfolio-app/src/components/page-footer/page-footer.tsx
+++ ux-portfolio-app/src/components/page-footer/page-footer.tsx
@@ -1,67 +1,12 @@
 import '../../css/globals.css'
-import './page-header.css'
-import { useState } from 'react'
-import { ScreenSizeResponse } from '../../tools/utilities.ts'
-import { Button } from "@fluentui/react-components";
-import { NavigationFilled } from '@fluentui/react-icons'
-import { StackShim, StackItemShim } from "@fluentui/react-migration-v8-v9";
+import './page-footer.css'
+import { StackShim } from "@fluentui/react-migration-v8-v9";
 
 function PageFooter() {
-  const [isNavOpen, setNavIsOpen] = useState(false);
-  const isTabletOrMobile = ScreenSizeResponse.IsTabletOrMobile();
-  const isDesktopOrLaptop = ScreenSizeResponse.IsDesktopOrLaptop();
-  const renderHamburger = () => {
-    return (
-        <Button icon={<NavigationFilled/>} title='hamburgerNavBtn' as='button' size='large' appearance='subtle' 
-          onClick = {()=>setNavIsOpen(!isNavOpen)} />
-    );
-  };
-
-  const renderVertNavList = () => {
-    const orientationClassAttr = "nav-menu-item-vert";
-    return renderNavigationList(false, orientationClassAttr)
-  }
-
-  const renderHorizNavList = () => {
-    const orientationClassAttr = "nav-menu-item-horiz";
-    return renderNavigationList(true, orientationClassAttr)
-  }
-
-  const renderNavigationList = (horizontal: boolean, orientationClassAttr: string) => {
-    const menuItems = ['Protoypes', 'Evaluations & Analysis', 'Papers', 'Artwork'];
-
-    return (
-    <StackShim className="expanded-nav-menu dashed-border rounded-border-std"
-      horizontal={horizontal}
-      verticalAlign = 'center'>
-        {menuItems.map( (value: string, index: number) => (
-          <StackShim key={"navItem"+value}
-            className={"nav-menu-item " + orientationClassAttr + (index == menuItems.length-1 ? "" : " divider")}>
-            <a href=''>{ value } </a>
-          </StackShim>))}
-    </StackShim>);
-  }
 
   return (
-    <StackShim>
-      <StackShim grow={1} horizontal={isTabletOrMobile} verticalAlign='center' className={'page-header ' + (isNavOpen ? 'drop-shadow': '')}
-        horizontalAlign={isTabletOrMobile ? 'space-evenly' : 'center'}>
-          <StackItemShim className="title-background">
-            <h1 className="title">
-              {isDesktopOrLaptop && 
-               <><span className='strawberry'>Jordyn</span><span className='sky'>Niara</span><span className='inchworm'>Smith</span></>}
-              {isTabletOrMobile &&
-                <><span className='strawberry'>J</span><span className='sky'>N</span><span className='inchworm'>S</span></>
-              }
-            </h1>
-          </StackItemShim>
-          { isTabletOrMobile && 
-          <StackItemShim>
-            { renderHamburger() }
-          </StackItemShim> 
-          }
-      </StackShim>
-      { isTabletOrMobile ? isNavOpen && renderVertNavList() : renderHorizNavList() }
+    <StackShim className="center footer" style={{background:"pebble"}}>
+      <div>Jordyn N. Smith © 2024</div>
     </StackShim>
   )
 }
--- ux-portfolio-app/src/pages/home/home.tsx
+++ ux-portfolio-app/src/pages/home/home.tsx
@@ -2,13 +2,14 @@ import '../../css/globals.css'
 import './home.css'
 import { StackShim} from "@fluentui/react-migration-v8-v9";
 import PageHeader  from "../../components/page-header/page-header"
+import PageFooter from '../../components/page-footer/page-footer';
 
 function Home() {
   return (
     <StackShim>
       {PageHeader()}
       {/*Body*/}
-      {/*Footer*/}
+      {PageFooter()}
     </StackShim>
   )
 }
