25fc7eda4b347571ca78084be3ffdeb8c475a992
jordynniara <jnsmith96@yahoo.com>
2024-12-09 00:25:09 -0500

    setup routing

---
+++ ux-portfolio-app/src/pages/artwork/artwork.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+const Artwork = () => {
+    return (
+        <h1>Artwork</h1>
+    )
+}
+
+export default Artwork
---
+++ ux-portfolio-app/src/pages/papers/papers.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+const Papers = () => {
+    return (
+        <h1>Papers</h1>
+    )
+}
+
+export default Papers
