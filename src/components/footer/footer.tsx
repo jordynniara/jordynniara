e1b8d541fa78dcbefb0c6a5ad759f06552bb1aea
jordynniara <jnsmith96@yahoo.com>
2024-12-19 22:16:31 -0500

    move footer padding to css

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -3,7 +3,7 @@ import './footer.css'
 
 function Footer() {
   return (
-    <div className="center footer leather-texture" style={{paddingTop:"5px"}}>
+    <div className="center footer leather-texture">
       <div>Jordyn N. Smith © 2024</div>
     </div>
   )
