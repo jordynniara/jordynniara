42ea852a38bd0c4e6d0e564e626ca95919bcc955
jordynniara <jnsmith96@yahoo.com>
2026-01-22 00:21:44 -0500

    fix impact card

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -189,7 +189,7 @@ const ImpactCard = () => {
           </span>
           . Click here to learn more about it:
         </p>
-        <LinkButton to="/creativity-project" variant="inchworm">
+        <LinkButton to="/creativity-project" variant="inchworm" className="mx-auto flex justify-center text-white w-sm">
           The Creativity Project
         </LinkButton>
       </Card>
