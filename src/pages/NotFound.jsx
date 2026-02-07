a66b66f8cda862cfb15c5e1e0c7e2dd6b726e8cc
jordynniara <jnsmith96@yahoo.com>
2026-02-07 07:30:13 -0500

    increase modal content width; adjust not found page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -3,8 +3,9 @@ import { Link } from 'react-router';
 
 export const NotFound = () => {
   return (
-    <div className="">
-      <img src={excuseMe} alt="Excuse Me" className="mx-auto max-h-screen max-w-full object-contain" />
+    <div className="flex flex-col items-center justify-center p-4">
+      <h1 className="text-h1 font-title md:text-title text-wetsoil mb-1 sm:mb-2">Page Not Found</h1>
+      <img src={excuseMe} alt="Excuse Me" className="mx-auto h-auto max-h-[60vh] w-full object-contain" />
     </div>
   )
 }
